204cce2c83a12df19c3789889f449ba1e1329851
sergio-code <115664238+sergio-code@users.noreply.github.com>
2024-11-05 21:04:21 -0300

    update
    Actualización

--- varios.js
+++ varios.js
@@ -1,4 +1,5 @@
 // funcion para escuchar un evento
+document.addEventListener('DOMContentLoaded', iniciarFormulario);
 function iniciarFormulario() {
     const form = document.getElementById('mordazas'); // toma el formulario por id
     if (form) {
@@ -12,9 +13,9 @@ function enviarDatos(event) {
     event.preventDefault(); // Evita el envío del formulario
 
     // valores del formulario
-    const maquina = document.getElementById('ingrese numero de maquina').value; //importante naturalizar sintaxis, se usa asi siempre para tomar elementos
-    const Operacion = document.getElementById('ingrese numero de operacion').value;
-    const modelo = document.getElementById('ingrese modelo').value;
+    const maquina = document.getElementById('maquina').value; //importante naturalizar sintaxis, se usa asi siempre para tomar elementos
+    const operacion = document.getElementById('operacion').value;
+    const modelo = document.getElementById('modelo').value;
 
     // objeto con los datos
     const data = { maquina, operacion, modelo };
@@ -22,42 +23,9 @@ function enviarDatos(event) {
 
     // respuesta exitosa
     console.log('Datos enviados correctamente'); // imprime en consola, se ve apretando f12 en el navegador y yendo a consola
-  
+    alert ('datos correctos. Maquina: '+ maquina + ',' + 'operacion: '+ operacion +', '+' modelo: '+ modelo);
+    window.location.href = 'bienvenido.html';
 
     }
 
- //DOCUMENTO JAVSCRIPT #2
-function enviarDatos2(event) {
-    event.preventDefault(); // Evitar el envío del formulario por defecto
-
-    // Obtener los valores del formulario
-    const maquina = document.getElementById('ingrese numero de maquina').value; //importante naturalizar sintaxis, se usa asi siempre para tomar elementos
-    const Operacion = document.getElementById('ingrese numero de operacion').value;
-    const modelo = document.getElementById('ingrese modelo').value;
-
-    // Crear un objeto con los datos
-    const data = { maquina, operacion, modelo };
-    console.log(data);
-
-    // Enviar los datos al servidor
-    fetch('/logueo', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Error en la solicitud');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Éxito:', data);
-        alert(data.message); // Mostrar mensaje de éxito
-    })
-    .catch(() => {
-        alert('Error al instanciar los datos');
-    });
-}
+ 
