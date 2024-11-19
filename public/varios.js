b13046ef99bca48f99c152c2bbc90d7aeea702a7
sergio-code <115664238+sergio-code@users.noreply.github.com>
2024-11-19 20:04:13 -0300

    Update Js
    Agregado de funciones básicas

--- public/varios.js
+++ public/varios.js
@@ -12,7 +12,7 @@ function iniciarFormulario() {
 function enviarDatos(event) {
     event.preventDefault(); // Evita el envío del formulario
 
-    // valores del formulario
+    // Obtener los valores del formulario
     const maquina = document.getElementById('maquina').value; //importante naturalizar sintaxis, se usa asi siempre para tomar elementos
     const operacion = document.getElementById('operacion').value;
     const modelo = document.getElementById('modelo').value;
@@ -21,11 +21,46 @@ function enviarDatos(event) {
     const data = { maquina, operacion, modelo };
     console.log(data);  // Mostrar los datos en la consola
 
-    // respuesta exitosa
-    console.log('Datos enviados correctamente'); // imprime en consola, se ve apretando f12 en el navegador y yendo a consola
-    alert ('datos correctos. Maquina: '+ maquina + ',' + 'operacion: '+ operacion +', '+' modelo: '+ modelo);
-    window.location.href = 'bienvenido.html';
+           
+    // Enviar los datos al servidor
+    fetch('/busquedaRepuesto', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', // Asegúrate de que es JSON
+        },
+        body: JSON.stringify(data), // Convertir el objeto en una cadena JSON
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Credenciales incorrectas');
+        }
+        return response.text();
+    })
+    .then(data => {
+        window.location.href = '/bienvenido';
+    })
+    .catch(error => {
+        mostrarErrorModal(error.message);
+    });
+}
 
-    }
+// Función para mostrar el modal con el error
+function mostrarErrorModal(mensaje) {
+    const modal = document.getElementById('miModal');
+    const modalMessage = document.getElementById('modal-message');
+    modalMessage.textContent = mensaje;
+    modal.style.display = 'block'; // Mostrar el modal
+}
+
+// Función para cerrar el modal
+function cerrarModal() {
+    const modal = document.getElementById('miModal');
+    modal.style.display = 'none'; // Ocultar el modal
+}
+
+
+function volver() {
+    window.history.back();
+}
 
  
