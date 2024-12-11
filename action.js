08a6fe901b7b53b189376679d95253e7be9c9b2c
sergio-code <115664238+sergio-code@users.noreply.github.com>
2024-12-10 21:24:49 -0300

    Up date tablamodelo.db
    datos en action.js

---
+++ action.js
@@ -0,0 +1,34 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+// Ruta completa a la base de datos SQLite
+const dbPath = 'C:\\Desarrollo\\MORDAZAS\\MORDAZAS\\BD\\TablaModelo.db';  // Ruta absoluta a la base de datos SQLite
+
+function verificarCredenciales(maquina, operacion, modelo) {
+    return new Promise((resolve, reject) => {
+        // Conectar a la base de datos SQLite
+        const db = new sqlite3.Database(dbPath, (err) => {
+            if (err) {
+                return reject('Error al conectar a la base de datos: ' + err.message);
+            }
+        });
+
+        db.get('SELECT * FROM maquina WHERE operacion = ? AND modelo = ?', [maquina, operacion, modelo], (err, row) => {
+            if (err) {
+                db.close();
+                return reject('Error al consultar la base de datos: ' + err.message);
+            }
+
+            if (row) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+
+            // Cerrar la base de datos después de la consulta
+            db.close();
+        });
+    });
+}
+
+module.exports = { verificarCredenciales };
